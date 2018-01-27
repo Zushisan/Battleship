412e3b60bbc1346303b51484656faf15e2caf2e8
Zushisan <74838@supinfo.com>
2018-01-27 20:03:20 +0000

    initialize grid with correct tile ID, will refactor the for loops

--- battleships.js
+++ battleships.js
@@ -15,7 +15,6 @@ $(document).ready(function() {
   // {},
   // {},
   // {}
-
   // ];
 
   function createGrid(){
@@ -24,37 +23,77 @@ $(document).ready(function() {
       $('.board').append('<div class="row" id="'+seaTiles[1][i]+'"></div>');
     }
     // Create Display TILES in ROWS
-    for(let j = 0; j < seaTiles[1].length; j++){
-      $('.row').append('<div class="tile" id="'+seaTiles[0][j]+'"></div>');
+    for(let i = 0; i < seaTiles[1].length; i++){
+      $('#1.row').append('<div class="tile" id="1'+seaTiles[0][i]+'"></div>');
+    }
+    for(let i = 0; i < seaTiles[1].length; i++){
+      $('#2.row').append('<div class="tile" id="2'+seaTiles[0][i]+'"></div>');
+    }
+    for(let i = 0; i < seaTiles[1].length; i++){
+      $('#3.row').append('<div class="tile" id="3'+seaTiles[0][i]+'"></div>');
+    }
+    for(let i = 0; i < seaTiles[1].length; i++){
+      $('#4.row').append('<div class="tile" id="4'+seaTiles[0][i]+'"></div>');
+    }
+    for(let i = 0; i < seaTiles[1].length; i++){
+      $('#5.row').append('<div class="tile" id="5'+seaTiles[0][i]+'"></div>');
+    }
+    for(let i = 0; i < seaTiles[1].length; i++){
+      $('#6.row').append('<div class="tile" id="6'+seaTiles[0][i]+'"></div>');
+    }
+    for(let i = 0; i < seaTiles[1].length; i++){
+      $('#7.row').append('<div class="tile" id="7'+seaTiles[0][i]+'"></div>');
+    }
+    for(let i = 0; i < seaTiles[1].length; i++){
+      $('#8.row').append('<div class="tile" id="8'+seaTiles[0][i]+'"></div>');
+    }
+    for(let i = 0; i < seaTiles[1].length; i++){
+      $('#9.row').append('<div class="tile" id="9'+seaTiles[0][i]+'"></div>');
+    }
+    for(let i = 0; i < seaTiles[1].length; i++){
+      $('#10.row').append('<div class="tile" id="10'+seaTiles[0][i]+'"></div>');
     }
   }
+
   // We initialize the grid, will refactor for two players and IA
   createGrid();
 
 
-  function tileClicked(rowID, columnID){
-    return columnID+rowID;
-  }
 
-  $('.row').click(function () {
-    }).children().click(function () {
-        let columnID = this.id;
-        let rowObject = $(this).parent('.row');
-        let rowID = rowObject.attr('id');
-        tileClicked(rowID, columnID);
-        });
+    // $('.row').click(function () {
+    // }).children().click(function () {
+    //   let columnID = this.id;
+    //   let rowObject = $(this).parent('.row');
+    //   let rowID = rowObject.attr('id');
+    //   console.log('I am messing stuff');
+    // }).attr('id', this.id+$(this).parent('.row').attr('id'));
+
+    // $('#your_element').attr('id', columnID+rowID);
+
+
+  // function tileClicked(rowID, columnID){
+  //   return columnID+rowID;
+  // }
+
+  // $('.row').click(function () {
+  // }).children().click(function () {
+  //   let columnID = this.id;
+  //   let rowObject = $(this).parent('.row');
+  //   let rowID = rowObject.attr('id');
+  //   tileClicked(rowID, columnID);
+  //   });
 
 
   $('#destroyer').click(function(){
     console.log("i click button");
 
-      $('.row').on('mouseover', function () {
+    $('.row').on('mouseover', function () {
     }).children().on('mouseover', function () {
-        $(this).addClass('hover');
-        $(this).next().addClass('hover');
+      $(this).addClass('hover');
+      $(this).next().addClass('hover');
 
 
-        }).mouseout(function(){
+    }).mouseout(function(){
       $(this).removeClass('hover');
       $(this).next().removeClass('hover');
 
@@ -62,10 +101,10 @@ $(document).ready(function() {
   });
 
     $('#carrier').click(function(){
-    console.log("i click button");
+      console.log("i click button");
 
       $('.row').on('mouseover', function () {
-    }).children().on('mouseover', function () {
+      }).children().on('mouseover', function () {
         $(this).addClass('hover');
         $(this).next().addClass('hover');
         $(this).next().next().addClass('hover');
@@ -73,23 +112,25 @@ $(document).ready(function() {
         $(this).prev().prev().addClass('hover');
 
 
-        }).mouseout(function(){
-      $(this).removeClass('hover');
-      $(this).next().removeClass('hover');
-      $(this).next().next().removeClass('hover');
-      $(this).prev().removeClass('hover');
-      $(this).prev().prev().removeClass('hover');
+      }).mouseout(function(){
+        $(this).removeClass('hover');
+        $(this).next().removeClass('hover');
+        $(this).next().next().removeClass('hover');
+        $(this).prev().removeClass('hover');
+        $(this).prev().prev().removeClass('hover');
 
-    });;
-  });
+      });;
+    });
 
-        $('#carrierVertical').click(function(){
-    console.log("i click button");
+    $('#carrierVertical').click(function(){
+      console.log("i click button");
 
       $('.row').on('mouseover', function () {
-        let idtest = $(this).children('.tile').attr('id');
+
+        let idtest = $(this).children('.tile').next().attr('id');
+        console.log(idtest);
         $(this).children('#'+idtest).addClass('hover');
-    }).children().on('mouseover', function () {
+      }).children().on('mouseover', function () {
         // $(this).addClass('hover');
         // $(this).next().addClass('hover');
         // $(this).next().next().addClass('hover');
@@ -97,15 +138,15 @@ $(document).ready(function() {
         // $(this).prev().prev().addClass('hover');
 
 
-        }).mouseout(function(){
+      }).mouseout(function(){
       // $(this).removeClass('hover');
       // $(this).next().removeClass('hover');
       // $(this).next().next().removeClass('hover');
       // $(this).prev().removeClass('hover');
       // $(this).prev().prev().removeClass('hover');
 
-    });;
-  });
+      });;
+    });
 
 
 
