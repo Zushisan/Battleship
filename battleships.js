6ad3a33a9b36482b957a829b992d54ab7d314e94
Zushisan <74838@supinfo.com>
2018-01-27 20:41:26 +0000

    set some working kind of vertical setup for ship placement

--- battleships.js
+++ battleships.js
@@ -58,30 +58,17 @@ $(document).ready(function() {
   // We initialize the grid, will refactor for two players and IA
   createGrid();
 
+  // That function will return the vertical ID from this and 'number' tile away
+  // Use it to set ship vertically
+  function setVerticalId(thisId, number){
+    let verticalId = $(thisId).attr('id');
+    let changeId = verticalId.split('');
+    changeId[0] = parseInt(changeId);
+    changeId[0] = changeId[0] + number;
+    verticalId = changeId.join('');
+    return '#' + verticalId;
+  }
 
-
-    // $('.row').click(function () {
-    // }).children().click(function () {
-    //   let columnID = this.id;
-    //   let rowObject = $(this).parent('.row');
-    //   let rowID = rowObject.attr('id');
-    //   console.log('I am messing stuff');
-    // }).attr('id', this.id+$(this).parent('.row').attr('id'));
-
-    // $('#your_element').attr('id', columnID+rowID);
-
-
-  // function tileClicked(rowID, columnID){
-  //   return columnID+rowID;
-  // }
-
-  // $('.row').click(function () {
-  // }).children().click(function () {
-  //   let columnID = this.id;
-  //   let rowObject = $(this).parent('.row');
-  //   let rowID = rowObject.attr('id');
-  //   tileClicked(rowID, columnID);
-  //   });
 
 
   $('#destroyer').click(function(){
@@ -123,31 +110,28 @@ $(document).ready(function() {
     });
 
     $('#carrierVertical').click(function(){
-      console.log("i click button");
 
-      $('.row').on('mouseover', function () {
-
-        let idtest = $(this).children('.tile').next().attr('id');
-        console.log(idtest);
-        $(this).children('#'+idtest).addClass('hover');
-      }).children().on('mouseover', function () {
-        // $(this).addClass('hover');
-        // $(this).next().addClass('hover');
-        // $(this).next().next().addClass('hover');
-        // $(this).prev().addClass('hover');
-        // $(this).prev().prev().addClass('hover');
-
+      $('.tile').on('mouseover', function () {
+        $(this).addClass('hover');
+        $(setVerticalId(this, 1)).addClass('hover');
+        $(setVerticalId(this, 2)).addClass('hover');
+        $(setVerticalId(this, 3)).addClass('hover');
+        $(setVerticalId(this, 4)).addClass('hover');
 
       }).mouseout(function(){
-      // $(this).removeClass('hover');
-      // $(this).next().removeClass('hover');
-      // $(this).next().next().removeClass('hover');
-      // $(this).prev().removeClass('hover');
-      // $(this).prev().prev().removeClass('hover');
+        $(this).removeClass('hover');
+        $(setVerticalId(this, 1)).removeClass('hover');
+        $(setVerticalId(this, 2)).removeClass('hover');
+        $(setVerticalId(this, 3)).removeClass('hover');
+        $(setVerticalId(this, 4)).removeClass('hover');
 
       });;
     });
-
-
-
 });
+
+
+
+
+
+
+
